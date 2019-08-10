574ed3385b8c9d17000d60313ec35f285df3db84
velociveloci <marskw@op.pl>
2019-08-10 09:15:32 +0200

    first

---
+++ functions.php
@@ -0,0 +1,96 @@
+<?php
+
+function pageBanner($args = NULL) {
+	
+	if (!$args['title']) {
+		$args['title'] = get_the_title();
+	}
+
+	if (!$args['subtitle']) {
+		$args['subtitle'] = get_field('page_banner_subtitle');
+	}
+
+	if(!$args['photo']) { 
+		if(get_field('page_banner_background_image')) {
+			$args['photo'] = get_field('page_banner_background_image')['url'];
+		}
+		else {
+			$args['photo'] = get_theme_file_uri('images/ocean.jpg');
+		}
+	}
+
+/*
+
+	if (!$args['photo']) {
+		if(get_field('page_banner_background_image')) {
+			$args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+		} else {
+			$args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+		}
+	}
+*/
+
+	?>
+
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>  
+  </div>
+
+<?php } 
+
+function university_files() {
+	wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, 1.0, true); /* delete after site live - microtime() and change to 1.0  */
+	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+	wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime()); /* delete after site live - , NULL, microtime()  */
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+
+	/*  register_nav_menu('headerMenuLocation', 'Header Menu Location' );  adding menu in admin left panel menu - to hide it, just delete or hide this line */
+	/*  register_nav_menu('footerLocationOne', 'Footer Location One' );  adding first footer menu in admin left panel menu - to hide it, just delete or hide this line */
+	/*  register_nav_menu('footerLocationTwo', 'Footer Location Two' );  adding second footer menu in admin left panel menu - to hide it, just delete or hide this line */
+
+	add_theme_support('title-tag'); /* automatically changing page title in tag */
+	add_theme_support('post-thumbnails');
+	add_image_size('professor_landscape', 400, 260, true);
+	add_image_size('professor_portrait', 480, 650, true);
+	add_image_size('page_banner', 1500, 350, true);
+
+}
+
+add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query) {
+	if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+		$query->set('orderby', 'title');
+		$query->set('order', 'ASC');
+		$query->set('post_per_pages', -1);
+	}
+
+
+	if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+		$today = date('Ymd');
+		$query->set('meta_key', 'event_date');
+		$query->set('orderby', 'meta_value_num');
+		$query->set('order', 'ASC');
+		$query->set('meta_query', array(
+			array(
+				'key' => 'event_date',
+				'compare' => '>=',
+				'value' => $today,
+				'type' => 'numeric'
+			)
+		));
+	}
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
